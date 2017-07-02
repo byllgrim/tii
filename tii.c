09b3c4e1503381772624ad3c662a09259cf790f5
byllgrim <robin.a.t.pedersen@gmail.com>
2017-07-02 21:13:41 +0200

    send messages

--- tii.c
+++ tii.c
@@ -17,7 +17,7 @@ struct channel {
 };
 
 static void
-print_channels(struct channel *ch)
+print_channels(struct channel *ch) /* TODO assuming MAXCH */
 {
 	size_t i;
 
@@ -26,9 +26,40 @@ print_channels(struct channel *ch)
 		if (ch[i].selected)
 			printf("[%s]", ch[i].name);
 		else
-			printf(" %s ", ch[i].name);
+			printf(" %s%c",
+			       ch[i].name,
+			       ch[i].notify ? '*' : ' ');
 	}
 	puts("");
+}
+
+static void
+send_input(char *msg, size_t n, struct channel *ch, size_t i)
+{
+	FILE *f;
+	char p[BUFSIZ]; /* TODO too big size */
+
+	p[0] = '\0';
+	strcat(p, "./");
+	strcat(p, ch[0].name);
+	strcat(p, "/in");
+	/* strcat(p, ""); TOOD if !i */
+	(void)i; /* TODO don't default to 0 */
+	if (!(f = fopen(p, "a"))) {
+		printf("send_input: failed to open file\n");
+		/* TODO proper error checking */
+		return;
+	}
+
+	/* TODO proper error checking */
+	if (fwrite(msg, sizeof(char), n, f))
+		printf("sent success\n");
+	else
+		printf("boo\n");
+
+	fwrite("\n", 1, 1, f); /* TODO don't strip \n */
+	printf("sent '%s' to '%s'\n", msg, p);
+	fclose(f);
 }
 
 int
@@ -39,7 +70,9 @@ main(void)
 	struct channel ch[MAXCHN] = {0};
 	size_t i;
 	char cmd[BUFSIZ];
+	char in[BUFSIZ];
 
+	/* TODO refactor into parse_dirs() */
 	d = opendir("./"); /* TODO error checking */
 	ch[0].name[0] = '\0';
 	for (i = 0; (ds = readdir(d)); ) {
@@ -62,17 +95,22 @@ main(void)
 	}
 	ch[0].selected = 1; /* TODO use index instead */
 
+	/* TODO refactor */
 	while (1) {
 		cmd[0] = '\0';
 		strcat(cmd, "tail -n24 ./"); /* TODO -n = ? */
 		strcat(cmd, ch[0].name);
 		strcat(cmd, "/out");
 		system(cmd);
+		/* TODO use read() */
 
 		print_channels(ch);
 
 		printf("in: ");
-		getchar();
+		fgets(in, BUFSIZ, stdin);
+		in[strlen(in) - 1] = '\0';
+		if (in[0])
+			send_input(in, strlen(in), ch, 0);
 		/* TODO input don't block output */
 	}
 
