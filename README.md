6b2dfe0854fc69bc9bf434c6bd95b2041073c98e
byllgrim <robin.a.t.pedersen@gmail.com>
2017-07-02 22:39:44 +0200

    add shortkeys to change channel view

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Usage:
 `cd` into the `irc` directory and launch `tii`.
 The `out` file will be shown in terminal, along with the selected channel, and
 an input line at the bottom.
+Next channel ^L, prev channel ^H.
 
 Notes:
 This is hastily written.
--- tii.c
+++ tii.c
@@ -4,10 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <termios.h>
+#include <unistd.h>
 
 enum {
 	NAMELEN = 128,
-	MAXCHN = 16
+	MAXCHN = 16,
+	CTRL_L = 0xC, /* TODO use '^L' */
+	CTRL_H = 0x8
 };
 
 struct channel {
@@ -45,6 +49,8 @@ send_input(char *msg, size_t n, struct channel *ch, size_t i)
 	strcat(p, "/in");
 	/* strcat(p, ""); TOOD if !i */
 	(void)i; /* TODO don't default to 0 */
+	/* TODO check if file exist */
+	/* TODO check if ii is running */
 	if (!(f = fopen(p, "a"))) {
 		printf("send_input: failed to open file\n");
 		/* TODO proper error checking */
@@ -60,6 +66,47 @@ send_input(char *msg, size_t n, struct channel *ch, size_t i)
 	fwrite("\n", 1, 1, f); /* TODO don't strip \n */
 	printf("sent '%s' to '%s'\n", msg, p);
 	fclose(f);
+}
+
+static void
+get_input(char *buf, size_t n)
+{
+	char c;
+	size_t i;
+
+	for (i = 0; i < n - 1; i++) {
+		c = getchar(); /* TODO don't block output */
+
+		if (c == CTRL_L) {
+			printf("next channel\n");
+			i--;
+			continue;
+		} /* TODO else */
+
+		if (c == CTRL_H) {
+			printf("prev channel\n");
+			i--;
+			continue;
+		}
+
+		if (c == '\n')
+			c = '\0';
+
+		buf[i] = c;
+
+		if (c == '\0')
+			return; /* TODO better logic */
+	}
+}
+
+static void
+non_canonical(void)
+{
+	struct termios attr;
+
+	tcgetattr(0, &attr); /* TODO no magic numbers */
+	attr.c_lflag &= ~ICANON;
+	tcsetattr(0, TCSANOW, &attr);
 }
 
 int
@@ -96,6 +143,7 @@ main(void)
 	ch[0].selected = 1; /* TODO use index instead */
 
 	/* TODO refactor */
+	non_canonical();
 	while (1) {
 		cmd[0] = '\0';
 		strcat(cmd, "tail -n24 ./"); /* TODO -n = ? */
@@ -107,8 +155,7 @@ main(void)
 		print_channels(ch);
 
 		printf("in: ");
-		fgets(in, BUFSIZ, stdin);
-		in[strlen(in) - 1] = '\0';
+		get_input(in, sizeof(in));
 		if (in[0])
 			send_input(in, strlen(in), ch, 0);
 		/* TODO input don't block output */
